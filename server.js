8dc3cced405b8f0f309b9731062d881ed7addfcd
dronkillerShark <88827209+dronkillerShark@users.noreply.github.com>
2021-12-21 06:13:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/circleGame', (req, res) => {
-    res.sendFile('/Users/jameelgmail.com/Downloads/projects/project7/circleGame.html');
+    res.sendFile(__dirname + '/circleGame.html');
 })
 
 app.get('/pingPong', (req, res) => {
-  res.sendFile('/Users/jameelgmail.com/Downloads/projects/project7/pingPong.html');
+  res.sendFile(__dirname + '/pingPong.html');
 })
 
 app.get('/', (req, res) => {
-  res.sendFile('/Users/jameelgmail.com/Downloads/projects/project7/HomePage.html');
+  res.sendFile(__dirname + '/HomePage.html');
 })
 
 app.get('*', (req, res) =>{
